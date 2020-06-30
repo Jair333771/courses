b897b706b1078ddac58133f5eb84e3255d140eae
JairR <jair.rodriguez@wunderman.com>
2020-06-29 21:58:06 -0500

    Add SwaggerConfiguration

--- Services/Startup.cs
+++ Services/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Services.Swagger;
+using Swashbuckle.Swagger;
 
 namespace Services
 {
@@ -54,11 +56,34 @@ namespace Services
                       .AllowAnyMethod();
                 });
             });
+
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc(SwaggerConfiguration.DocNameV1,
+                    new Microsoft.OpenApi.Models.OpenApiInfo
+                    {
+                        Title = SwaggerConfiguration.DocInfoTitle,
+                        Version = SwaggerConfiguration.DocInfoVersion,
+                        Description = SwaggerConfiguration.DocInfoDescription,
+                        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                        {
+                            Name = SwaggerConfiguration.ContactName,
+                            Url = new Uri(SwaggerConfiguration.ContactUrl)
+                        }
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(SwaggerConfiguration.EndpointUrl, SwaggerConfiguration.EndpointDescription);
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -68,6 +93,8 @@ namespace Services
 
             app.UseRouting();
 
+            app.UseCors(cors);
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -75,7 +102,7 @@ namespace Services
                 endpoints.MapControllers();
             });
 
-            app.UseCors(cors);
+
         }
     }
 }
---
+++ Services/Swagger/SwaggerConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.Swagger
+{
+    public class SwaggerConfiguration
+    {
+        /// <summary>
+        /// <para>Foo API v1</para>
+        /// </summary>
+        public const string EndpointDescription = "Course v1";
+
+        /// <summary>
+        /// <para>/swagger/v1/swagger.json</para>
+        /// </summary>
+        public const string EndpointUrl = "/swagger/v1/swagger.json";
+
+        /// <summary>
+        /// <para>Jorge Serrano</para>
+        /// </summary>
+        public const string ContactName = "Jair Rodriguez";
+
+        /// <summary>
+        /// <para>http://geeks.ms/jorge/</para>
+        /// </summary>
+        public const string ContactUrl = "https://www.linkedin.com/in/jair-rodr%C3%ADguez-91830819a/";
+
+        /// <summary>
+        /// <para>v1</para>
+        /// </summary>
+        public const string DocNameV1 = "v1";
+
+        /// <summary>
+        /// <para>Foo API</para>
+        /// </summary>
+        public const string DocInfoTitle = "Courses - Amis";
+
+        /// <summary>
+        /// <para>v1</para>
+        /// </summary>
+        public const string DocInfoVersion = "v1";
+
+        /// <summary>
+        /// <para>Foo Api - Sample Web API in ASP.NET Core 2</para>
+        /// </summary>
+        public const string DocInfoDescription = "Prueba NetCore 3.1 con  Angular";
+
+    }
+}
